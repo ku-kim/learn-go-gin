d9a76b845f03dec7c41299f819b0bf8ab1351b86
kukim <kukim.kim@groundx.xyz>
2023-03-16 18:13:49 +0900

    custom middleware 수정

--- middleware-ex/custom-middleware.go
+++ middleware-ex/custom-middleware.go
@@ -9,17 +9,23 @@ import (
 // Custom mildderware : https://gin-gonic.com/ko-kr/docs/examples/custom-middleware/
 func CustomLoggerMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// Request 이전
 		t := time.Now()
-
 		c.Set("example", 12345)
-		time.Sleep(3 * time.Second)
+		log.Print("c.Next 이전, 2초 대기 . . .")
+		time.Sleep(2 * time.Second)
+		log.Print("2초 끝")
 
 		c.Next() // Request 기준
-		// Request 이후
-		latency := time.Since(t)
-		log.Print(latency)
 
-		// 송싱할 상태 코드에 접근
+		// Request 이후
+		log.Print("c.next 이후, 3초 대기 . .")
+		latency := time.Since(t)
+		time.Sleep(3 * time.Second)
+		log.Print(latency)
+		log.Print("3초 끝")
+
+		// response status code 접근
 		status := c.Writer.Status()
 		log.Println(status)
 	}
